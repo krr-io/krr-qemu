274b3007e5dd76654b3fd59ed0bb14230c8383e6
Silver Zhang <silvertianren@gmail.com>
2024-03-28 10:12:49 -0400

    Add automated script for testing

---
+++ kernel_rr/observer.py
@@ -0,0 +1,276 @@
+import os
+import traceback
+import pandas as pd
+import subprocess
+import argparse
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+
+
+DATA_DIR = "test_data"
+
+
+REDIS_TP = "redis-tp"
+AVG_LAT = "avg-latency"
+THROUGHPUT = "throughput"
+OPSPS = "opsps"
+LATENCY = "latency"
+
+
+ROCKS_DB_BP_TEST_NAME = "rocksdb_kernel_bypass"
+
+ROCKS_DB_NBP_TEST_NAME = "rocksdb"
+REDIS_TEST_NAME = "redis"
+
+mode = "kernel_rr"
+test_name = REDIS_TEST_NAME
+
+modes = {"kernel_rr": 0, "baseline": 1}
+
+metrics = {
+    ROCKS_DB_BP_TEST_NAME: (THROUGHPUT, OPSPS, LATENCY)
+}
+
+benchmarks = {
+    ROCKS_DB_BP_TEST_NAME: ("fillseq", "fillrandom", "readseq", "readrandom"),
+    ROCKS_DB_NBP_TEST_NAME: ("fillseq", "fillrandom", "readseq", "readrandom")
+}
+
+cpu_nums = ["1", "2", "4", "8", "16"]
+current_cpu_num = 1
+
+
+def get_file_name(benchmark, metric):
+    return "test_data/{}-{}-{}.csv".format(test_name, benchmark, metric)
+
+
+def init_csv_file(benchmark, metric):
+    with open(get_file_name(benchmark, metric), 'w') as f:
+        f.write("cores,mode,value")
+
+
+def append_file(benchmark, metric, value):
+    file = get_file_name(benchmark, metric)
+
+    if not os.path.exists(file):
+        init_csv_file(benchmark, metric)
+
+    df = pd.read_csv(file)
+    row = [current_cpu_num, mode, value]
+
+    df.loc[len(df)] = row
+    df.to_csv(file, index=False)
+
+def generate_rocksdb_bp(buffer):
+    tp = 0
+    ops_ps = 0
+    latency = 0
+    item_list = buffer.split()
+
+    print("Getting the data")
+
+    try:
+        fillseq = "fillseq" in buffer
+        fillrandom = "fillrandom" in buffer
+        readseq = "readseq" in buffer
+        readrandom = "readrandom" in buffer
+        if fillseq or fillrandom:
+            tp = item_list[6]
+            ops_ps = item_list[4]
+            latency = item_list[2]
+
+            bm = "unknown"
+            if fillseq:
+                bm = "fillseq"
+            elif fillrandom:
+                bm = "fillrandom"
+            
+            append_file(bm, THROUGHPUT, tp)
+        elif readseq or readrandom:
+            ops_ps = item_list[4]
+            latency = item_list[2]
+
+            if readrandom:
+                bm = "readrandom"
+            elif readseq:
+                bm = "readseq"
+
+        append_file(bm, OPSPS, ops_ps)
+        append_file(bm, LATENCY, latency)
+
+    except Exception as e:
+        print("{} {}: {}".format(traceback.format_exc(), e, item_list))
+
+
+def generate_redis(buffer, benchmark, latency=False):
+    if "throughput" in buffer:
+        item_list = buffer.split()
+        tp = item_list[3]
+
+        append_file(benchmark, REDIS_TP, tp)
+
+    if latency:
+        item_list = buffer.split()
+        avg_lat = item_list[0]
+        append_file(benchmark, AVG_LAT, avg_lat)
+
+
+def generate_values_rocksdb(buffer):
+    if test_name in (ROCKS_DB_BP_TEST_NAME, ROCKS_DB_NBP_TEST_NAME):
+        generate_rocksdb_bp(buffer)
+
+
+def get_data_redis():
+    with open("./rr-result.txt", "r") as f:
+        content = f.read()
+        bm = ""
+        if "GET" in content:
+            bm = "get"
+        elif "SET" in content:
+            bm="set"
+
+        lines = content.split("\n")
+
+        for index, line in enumerate(lines):
+            if "avg" in line:
+                generate_redis(lines[index+1], bm, True)
+            else:
+                generate_redis(line, bm, False)
+
+def get_data_rocksdb():
+    with open("./rr-result.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            generate_values_rocksdb(line)
+
+def get_data():
+    if test_name in (ROCKS_DB_BP_TEST_NAME, ROCKS_DB_NBP_TEST_NAME):
+        get_data_rocksdb()
+    elif test_name == REDIS_TEST_NAME:
+        get_data_redis()
+
+# init_csv_file()
+
+qemu_binary = "../build/qemu-system-x86_64"
+
+def test_run(cpu_num):
+    global current_cpu_num
+    
+    current_cpu_num = cpu_num
+
+    extra_dev = ""
+    disk_image = os.environ["KRR_DISK"]
+    ivshmem = ""
+
+    if mode == "kernel_rr":
+        kernel_image = os.environ["KRR_SMP_IMG"]
+
+        if cpu_num == "1":
+            kernel_image = os.environ["KRR_UNI_IMG"]
+
+        ivshmem = "-object memory-backend-file,size=32768M,share,mem-path=/dev/shm/ivshmem,id=hostmem -device ivshmem-plain,memdev=hostmem"
+    elif mode == "baseline":
+        kernel_image = os.environ["BL_IMG"]
+
+    if test_name == ROCKS_DB_BP_TEST_NAME:
+        extra_dev = " -drive file=../build/nvm.img,if=none,id=nvm -device nvme,serial=deadbeef,drive=nvm"
+
+    qemu_base_cmd = """
+    {qemu_binary} -kernel {kernel_image} \
+    -accel kvm -smp {cpu_num} -cpu host -no-hpet -m 8G -append \
+    "root=/dev/sda rw init=/lib/systemd/systemd tsc=unstable console=ttyS0" \
+    -hda {disk_image} \
+    {ivshmem} -vnc :00 -D rec.log {extra_dev} -exit-record 1
+    """.format(
+        qemu_binary=qemu_binary, kernel_image=kernel_image,
+        disk_image=disk_image, cpu_num=cpu_num,
+        ivshmem=ivshmem, extra_dev=extra_dev,
+    )
+
+    print("QEMU CMD: {}".format(qemu_base_cmd))
+
+    process = subprocess.Popen(qemu_base_cmd, shell=True)
+    rc = 0
+    cnt = 0
+
+    while True:
+        streamdata = process.communicate()[0]
+        if process.returncode > 0:
+            rc = process.returncode
+            break
+
+        time.sleep(1)
+        cnt += 1
+
+        if cnt > 60:
+            process.kill()
+            return -1
+
+    print("return code {}".format(rc))
+
+    get_data()
+
+    return 0
+
+
+def generate_graphs(path):
+    file_name = path.split("/")[-1].split(".")[0]
+
+    info_list = file_name.split("-")
+    test_name = info_list[0]
+    test = info_list[1]
+    metric = info_list[2]
+
+    metric2y = {
+        THROUGHPUT: "Throughput (MB/s)",
+        OPSPS: "Throughput (ops/s)",
+        LATENCY: "Latency (micros/op)",
+    }
+
+    df = pd.read_csv(path)
+
+    df['cores'] = df['cores'].astype(str)
+
+    ax = sns.lineplot(x='cores', y='value', hue='mode', data=df)
+    plt.xticks(df['cores'].unique())
+
+    sns.set_theme(style='white', font_scale=1)
+
+    plt.xlabel('CPU Number')
+    plt.ylabel(metric2y[metric])
+    ax.get_legend().remove()
+    plt.title('{}({})'.format(test_name, test), fontsize=12)
+    plt.legend(title='Mode', loc='best')
+    plt.tight_layout()
+    plt.savefig('{}/{}.png'.format(DATA_DIR, file_name), dpi=600)
+    plt.clf()
+    plt.close('all')
+
+
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+parser.add_argument("--mode", default="kernel_rr")
+parser.add_argument("--graph", default="false")
+parser.add_argument("--test", default=ROCKS_DB_BP_TEST_NAME)
+args = parser.parse_args()
+
+mode = args.mode
+test_name = args.test
+
+print("mode={}".format(mode))
+
+for cpu_num in cpu_nums:
+    while test_run(cpu_num) < 0:
+        print("Timeout try again")
+
+
+if args.graph == "true":
+    for file in os.listdir(DATA_DIR):
+        if not file.endswith(".csv"):
+            continue
+        generate_graphs("{}/{}".format(DATA_DIR, file))

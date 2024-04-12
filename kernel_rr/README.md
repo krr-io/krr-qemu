01fc3d6330be920900d5e05b91a4ac708646a85c
Silver Zhang <silvertianren@gmail.com>
2024-04-12 08:53:38 -0400

    Add doc for graph gen

--- kernel_rr/README.md
+++ kernel_rr/README.md
@@ -9,3 +9,10 @@
 3. Disabled KVM pvclock, by removing KVM_FEATURE_CLOCKSOURCE and KVM_FEATURE_CLOCKSOURCE2 features exposed to guest in KVM, so that the KVM won't update the guest memory, this is for us to do memory verification;
 
 4. Set the CPU_BASED_RDTSC_EXITING bit for VMX so that the RDTSC would be trapped to hypervisor.
+
+
+
+## Generating graph
+```
+python3 gratph_generator.py --graphtest=<e.g., rocksdb-readwhilewriting> --graphdir=<parent dir of the csv files>
+```
--- kernel_rr/graph_generator.py
+++ kernel_rr/graph_generator.py
@@ -4,10 +4,96 @@ import argparse
 import constants
 
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 graphdir = ""
+
+metric2y = {
+    constants.THROUGHPUT: "Throughput (MB/s)",
+    constants.OPSPS: "Throughput (ops/s)",
+    constants.LATENCY: "Latency (micros/op)",
+    constants.REDIS_TP: "Throughput (req/s)",
+    constants.AVG_LAT: "Average Latency (ms)"
+}
+
+
+def filter_valid_data(df, test):
+
+    for index, row in df.iterrows():
+        try:
+            val = float(row["value"])
+            if test == constants.THROUGHPUT and val > 100:
+                raise Exception("Value {} is wrong".format(val))
+        except:
+            print("drop invalid value: {}".format(row["value"]))
+            df.drop(index, inplace=True)
+
+
+def check_valid_data(df, test):
+    for index, row in df.iterrows():
+        try:
+            if test == constants.THROUGHPUT and float(row["value"]) > 100:
+                raise Exception("Value {} is wrong".format(row["value"]))
+        except Exception as e:
+            raise Exception("Failed compare: {}".format(row["value"]))
+
+
+def plug_single_graph(path):
+    file_name = path.split("/")[-1].split(".")[0]
+    file_dir = "/".join(path.split("/")[:-1])
+
+    info_list = file_name.split("-")
+    test_name = info_list[0]
+    test = info_list[1]
+    metric = info_list[2]
+
+    df = pd.read_csv(path)
+
+    palette = {'mean': 'orange', 'median': 'green', 'std': 'blue'}
+    df_data = df[(df["mode"] == "whole_system_rr")]
+
+    filter_valid_data(df_data, metric)
+
+    df_data['cores'] = pd.to_numeric(df['cores'], errors='coerce')
+    df_data['value'] = pd.to_numeric(df['value'], errors='coerce')
+
+    # check_valid_data(df_data, metric)
+
+    res_df = pd.DataFrame({"cores": [], "type": [], "value": []})
+    
+    for core in constants.CPU_NUMS:
+        d = df_data[(df_data["cores"] == int(core))]
+        # print(d["value"])
+
+        mean_row = [int(core), "mean", d["value"].mean()]
+        print(mean_row)
+
+        res_df.loc[len(res_df)] = mean_row
+
+        res_df.loc[len(res_df)] = [int(core), "median", d["value"].median()]
+        # print()
+        res_df.loc[len(res_df)] = [int(core), "std", d["value"].std()]
+
+
+    ax = sns.lineplot(x='cores', y='value', hue="type", data=res_df, linewidth=3, palette=palette)
+    # ax = sns.swarmplot(data=df_data, x='cores', y='value', size=3)
+    # sns.despine()
+    # sns.set_theme(style='white', font_scale=1.1)
+
+    plt.xlabel('Whole_system_rr-{}'.format(metric2y[metric]), fontsize=18, fontweight='normal')
+    # plt.ylabel(metric2y[metric], fontsize=18, fontweight='normal')
+    # # ax.get_legend().remove()
+
+    # plt.legend(title='Mode', loc='best')
+    # plt.tight_layout()
+    # plt.gca().set_ylim(bottom=0)
+    ax.set_ylim(bottom=0)
+    plt.savefig('{}/{}-temporary.png'.format(file_dir, file_name), dpi=600)
+
+    plt.clf()
+    plt.close('all')
 
 
 def generate_graphs(path):
@@ -18,14 +104,6 @@ def generate_graphs(path):
     test_name = info_list[0]
     test = info_list[1]
     metric = info_list[2]
-
-    metric2y = {
-        constants.THROUGHPUT: "Throughput (MB/s)",
-        constants.OPSPS: "Throughput (ops/s)",
-        constants.LATENCY: "Latency (micros/op)",
-        constants.REDIS_TP: "Throughput (req/s)",
-        constants.AVG_LAT: "Average Latency (ms)"
-    }
 
     print("Generating graph for {}".format(file_name))
 
@@ -39,6 +117,7 @@ def generate_graphs(path):
         for core in constants.CPU_NUMS:
             df_data = df[(df["mode"] == mode) & (df["cores"] == int(core))]
 
+            # Now we get mean of all trials on this setup
             value = df_data["value"].mean()
 
             row = [int(core), mode, value]
@@ -58,7 +137,6 @@ def generate_graphs(path):
     plt.ylabel(metric2y[metric], fontsize=18, fontweight='normal')
     ax.get_legend().remove()
 
-    # plt.title('{}({})'.format(test_name, test), fontsize=12)
     plt.legend(title='Mode', loc='best')
     plt.tight_layout()
     plt.gca().set_ylim(bottom=0)
@@ -75,7 +153,6 @@ parser = argparse.ArgumentParser(
                     epilog='Text at the bottom of help')
 
 parser.add_argument("--graphtest", default="all")
-parser.add_argument("--graphonly", default="false")
 parser.add_argument("--graphdir", default=constants.DATA_DIR)
 
 args = parser.parse_args()

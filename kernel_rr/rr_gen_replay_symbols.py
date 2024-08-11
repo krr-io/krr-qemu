acce159112e372b3165c942e8370a08f091740ea
Silver Zhang <silvertianren@gmail.com>
2024-08-11 08:13:17 -0400

    Make symbol generation compatible for more versions

--- kernel_rr/rr_gen_replay_symbols.py
+++ kernel_rr/rr_gen_replay_symbols.py
@@ -21,6 +21,11 @@ def filter_info_addr(symbol):
 
 def filter_loc_addr(loc):
     output = gdb.execute("b {}".format(loc), to_string=True)
+    lines = output.split("\n")
+    for line in lines:
+        if line.startswith("Breakpoint"):
+            output = line
+
     blocks = output.split()
 
     candidate = blocks[3][:-1]
@@ -45,8 +50,12 @@ def fetch_exc_page_fault():
     return filter_info_addr("exc_page_fault")
 
 def fetch_exc_page_fault_end():
-    if "6.1.0" in VERSION:
+    if "6.1.0" in VERSION or "6.1.18" in VERSION or "6.1.31" in VERSION:
         return filter_loc_addr("fault.c:1580")
+    elif "6.1.86" in VERSION:
+        return filter_loc_addr("fault.c:1523")
+    elif "6.1.61" in VERSION or "6.1.77" in VERSION:
+        return filter_loc_addr("fault.c:1532")
     else:
         return filter_loc_addr("fault.c:1463")
 
